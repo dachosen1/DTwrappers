2962b4e7a773bcb5b2ebdc35f25f8081e3448cf1
[dachosen1] <[anderson.nelson1@gmail.com]>
2019-11-03 15:01:54 -0500

    Small progress

--- R/total.missing.R
+++ R/total.missing.R
@@ -1,5 +1,7 @@
-# x:  a vector
-
-total.missing <- sum.missing <- function(x, ...){
+#' total missing
+#'
+#' @param x:  a vector
+#' @export
+total.missing <- sum.missing <- function(x, ...) {
   return(sum(is.na(x)))
 }
