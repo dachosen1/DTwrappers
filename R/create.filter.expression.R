b85173a4b71db22fd386fef62ff878fda158dd3e
Zoe0409 <46760923+Zoe0409@users.noreply.github.com>
2019-12-14 15:23:26 -0500

    First commit of this branch

--- R/create.filter.expression.R
+++ R/create.filter.expression.R
@@ -1,21 +1,20 @@
 #' create.filter.expression
 #' 
-#'  @description A function that creates filter expressions. Expression could be a character value, logical value, or expression stating the logical operations.
+#' @description A function that creates filter expressions. Expression could be a character value, logical value, or expression stating the logical operations.
 #' 
-#'  @param the.filter The filter criteria could be a character value, logical value, or expression stating the logical operations to be performed in filtering the data.
-#'  @import formulaic
-#'  @export
-#'  @examples 
+#' @param the.filter The filter criteria could be a character value, logical value, or expression stating the logical operations to be performed in filtering the data.
+#' @export
+#' @examples 
 #'  
-#'  age.name = "Age"
-#'  region.name = "Region"
+#' age.name = "Age"
+#' region.name = "Region"
 #'  
-#'  create.filter.expression(the.filter = NULL)
-#'  create.filter.expression(the.filter = c(age.name, region.name))
-#'  create.filter.expression(the.filter = age.name == region.name)
-#'  create.filter.expression(the.filter = "get(region.name) == 'South'")
-#'  create.filter.expression(the.filter = "get(age.name) > 20")
-#'  create.filter.expression(the.filter = "get(region.name) == 'South' & get(age.name) > 20")
+#' create.filter.expression(the.filter = NULL)
+#' create.filter.expression(the.filter = c(age.name, region.name))
+#' create.filter.expression(the.filter = age.name == region.name)
+#' create.filter.expression(the.filter = "get(region.name) == 'South'")
+#' create.filter.expression(the.filter = "get(age.name) > 20")
+#' create.filter.expression(the.filter = "get(region.name) == 'South' & get(age.name) > 20")
 #'  
 #' @export
 create.filter.expression <- function(the.filter){
--- R/dt.count.R
+++ R/dt.count.R
@@ -2,31 +2,40 @@
 #' 
 #' @description A function that counts the number of rows that meets the qualification such as filter and the by. If both filter and the by are not set up, then it will return the number of rows. 
 #' 
-#' @param dat:  a data.frame object.
-#' @param the.filter: a character value, logical value, or expression stating the logical operations to be performed in filtering the data prior to calculating the.function. Defaults to "NULL" unless specified.
-#' @param grouping.variables:  a character vector specifying variables to group by in performing the computation.  Only values that exist in names(dat) will be used.
-#' @param grouping.type:  a character value specifying whether the grouping should be sorted (keyby) or as is (by).  Defaults to keyby unless "by" is specified.
-#' @param count.name:  a character value specifying the name of the column of counts in the resulting table. Defaults to "N" unless specified.
+#' @param dat  a data.frame object.
+#' @param the.filter a character value, logical value, or expression stating the logical operations to be performed in filtering the data prior to calculating the.function. Defaults to "NULL" unless specified.
+#' @param grouping.variables  a character vector specifying variables to group by in performing the computation.  Only values that exist in names(dat) will be used.
+#' @param grouping.type  a character value specifying whether the grouping should be sorted (keyby) or as is (by).  Defaults to keyby unless "by" is specified.
+#' @param count.name  a character value specifying the name of the column of counts in the resulting table. Defaults to "N" unless specified.
 #' @note the data.frame dat will be converted to a data.table object to facilitate adding the new column by reference (e.g. efficiently with regard to the usage of memory)
 #' @import formulaic
 #' @export 
 #' @examples 
 #' 
-#'  data('snack.dat')
 #'  age.name = "Age"
 #'  income.name = "Income"
 #'  region.name = "Region"
 #'  gender.name = "Gender"
 #'  
-#'  dt.count(dat = snack.dat)
-#'  dt.count(dat = snack.dat, grouping.variables = c(region.name, gender.name))
-#'  dt.count(dat = snack.dat, the.filter = "Age > 65", grouping.variables = c(region.name, gender.name), count.name = "Records with Age > 65") 
-#'  dt.count(dat = snack.dat, the.filter = 'get(age.name) > 20', grouping.variables = c(age.name,income.name), grouping.type = 'keyby', count.name = 'count')
+#'  dt.count(dat = formulaic::snack.dat)
+#'  
+#'  dt.count(dat = formulaic::snack.dat, 
+#'  grouping.variables = c(region.name, gender.name))
+#'  
+#'  dt.count(dat = formulaic::snack.dat, 
+#'  the.filter = "Age > 65", 
+#'  grouping.variables = c(region.name, gender.name), 
+#'  count.name = "Records with Age > 65") 
+#'  
+#'  dt.count(dat = formulaic::snack.dat, 
+#'  the.filter = 'get(age.name) > 20', 
+#'  grouping.variables = c(age.name,income.name), 
+#'  grouping.type = 'keyby', count.name = "counts")
 #' 
 #' @source create.filter.expression.R
 #' @import data.table
 #' @export  
-dt.count <- function(dat, the.filter = NULL, grouping.variables = NULL, grouping.type = "keyby", count.name = "N", ...){
+dt.count <- function(dat, the.filter = NULL, grouping.variables = NULL, grouping.type = "keyby", count.name = "N"){
   
   data.table::setDT(dat)
   
