5a99ecd459a090e1e5161dc6621dbbabe1281416
Zoe0409 <46760923+Zoe0409@users.noreply.github.com>
2019-12-14 13:15:55 -0500

    Updated dt.define.variable

--- R/dt.define.variable.R
+++ R/dt.define.variable.R
@@ -5,13 +5,14 @@
 #' @param dat:  a data.frame object
 #' @param variable.name:  a character value specifying the name of the new column
 #' @param the.values: a vector of data specifying the values of the new column.
+#' @param specification: Defaults to "by.value" unless the.values is set to be an expression or a vector, then specification should be set as "by.expression".
+#' @param the.filter: a character value, logical value, or expression stating the logical operations to be performed in filtering the data prior to calculating the.function. Defaults to "NULL" unless specified.
 #' @note the data.frame dat will be converted to a data.table object to facilitate adding the new column by reference (e.g. efficiently with regard to the usage of memory)
-
+#' 
 #' @export 
-#' @import formulaic
-#' @source create.filter.expression.R
 #' @examples 
 #' 
+#' data('snack.dat')
 #' age.name = "Age"
 #' income.name = "Income"
 #' region.name = "Region"
@@ -25,10 +26,11 @@
 #' snack.dat[1:10, .SD, .SDcols = c(income.name, "Income in Thousands")]
 #' snack.dat <- dt.define.variable(dat = snack.dat, variable.name = "Region and Country", the.values = expression(sprintf('%s, USA', get(region.name))), specification = "by.expression")
 #' snack.dat[1:10, .SD, .SDcols = c(region.name, "Region and Country")]
-
+#' @import data.table
+#' @source create.filter.expression.R
+#' @export 
 dt.define.variable <- function(dat, variable.name, the.values, specification = "by.value", the.filter = NULL){
-  require(data.table)
-  setDT(dat)
+  data.table::setDT(dat)
   
   the.filter <- create.filter.expression(the.filter = the.filter)
   
