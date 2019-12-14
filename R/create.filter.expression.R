d9c6e9b2ac5788b2ffe6d0f5c33d8698465b57ed
Zoe0409 <46760923+Zoe0409@users.noreply.github.com>
2019-12-14 14:00:45 -0500

    Import Functions

---
+++ R/create.filter.expression.R
@@ -0,0 +1,31 @@
+#' create.filter.expression
+#' 
+#'  @description A function that creates filter expressions. Expression could be a character value, logical value, or expression stating the logical operations.
+#' 
+#'  @param the.filter The filter criteria could be a character value, logical value, or expression stating the logical operations to be performed in filtering the data.
+#'  @import formulaic
+#'  @export
+#'  @examples 
+#'  
+#'  age.name = "Age"
+#'  region.name = "Region"
+#'  
+#'  create.filter.expression(the.filter = NULL)
+#'  create.filter.expression(the.filter = c(age.name, region.name))
+#'  create.filter.expression(the.filter = age.name == region.name)
+#'  create.filter.expression(the.filter = "get(region.name) == 'South'")
+#'  create.filter.expression(the.filter = "get(age.name) > 20")
+#'  create.filter.expression(the.filter = "get(region.name) == 'South' & get(age.name) > 20")
+#'  
+#' @export
+create.filter.expression <- function(the.filter){
+  if(is.null(the.filter)){
+    the.filter <- TRUE
+  }
+  if(is.character(the.filter)){
+    the.filter <- parse(text = the.filter)
+  }
+  return(the.filter)
+}
+
+
