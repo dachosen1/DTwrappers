8fddcea6400efee3d4a60f8510bf7c8d669a714f
Zoe0409 <46760923+Zoe0409@users.noreply.github.com>
2019-12-18 11:42:37 -0500

    Rename the R scripts

--- R/dt.count_dt.filter_dt.select.R
+++ R/dt.count_dt.filter_dt.select.R
@@ -1,37 +1,3 @@
-#' create.filter.expression
-#'
-#' @description A function that creates filter expressions. Expression could be a character value,
-#' logical value, or expression stating the logical operations.
-#'
-#' @param the.filter The filter criteria could be a character value, logical value, or expression
-#' stating the logical operations to be performed in filtering the data.
-#'
-#' @export
-#' @examples
-#' age.name = "Age"
-#' region.name = "Region"
-#'
-#' create.filter.expression(the.filter = NULL)
-#' create.filter.expression(the.filter = c(age.name, region.name))
-#' create.filter.expression(the.filter = age.name == region.name)
-#' create.filter.expression(the.filter = "get(region.name) == 'South'")
-#' create.filter.expression(the.filter = "get(age.name) > 20")
-#' create.filter.expression(the.filter = "get(region.name) == 'South' & get(age.name) > 20")
-#' 
-#'@export
-create.filter.expression <- function(the.filter) {
-  if (is.null(the.filter)) {
-    the.filter <- TRUE
-  }
-  if (is.character(the.filter)) {
-    the.filter <- parse(text = the.filter)
-  }
-  return(the.filter)
-}
-
-
-
-
 #' dt.count
 #'
 #' @description A function that counts the number of rows that meets the qualification such as filter and the by (grouping variables). If both filter and the grouping variables are not set up, then it will return the number of rows.
