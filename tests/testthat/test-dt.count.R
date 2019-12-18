96494402371ceb55e3a17489fc92add80f8a8c3f
Zoe0409 <46760923+Zoe0409@users.noreply.github.com>
2019-12-18 14:09:21 -0500

    Update testthat in dt.count

--- tests/testthat/test-dt.count.R
+++ tests/testthat/test-dt.count.R
@@ -16,10 +16,10 @@ dt.count2 = dt.count(dat = dat, the.filter = "Age > 65", grouping.variables = c(
 
 test_that("dt.count works", {
   expect_equal(dt.count0$N, 23000)
-  expect_equal(dt.count1$Region, c("Midwest","Midwest","Northeast","Northeast","South","South","West","West"))
-  expect_equal(dt.count1$Gender, c("Female", "Male","Female", "Male","Female", "Male","Female", "Male"))
-  expect_equal(dt.count1$N, c(2139,2093,3588,3335,1679,1541,4278,4347))
-  expect_equal(dt.count2$Region, c("Midwest","Midwest","Northeast","Northeast","South","South","West","West"))
-  expect_equal(dt.count2$Gender, c("Female", "Male","Female", "Male","Female", "Male","Female", "Male"))
-  expect_equal(dt.count2$`Records with Age > 65`, c(736,897,1403,1265,575,483,1564,1518))
+  expect_equal(sum(dt.count1$Region != c("Midwest","Midwest","Northeast","Northeast","South","South","West","West")), 0)
+  expect_equal(sum(dt.count1$Gender != c("Female", "Male","Female", "Male","Female", "Male","Female", "Male")), 0)
+  expect_equal(sum(dt.count1$N != c(2139,2093,3588,3335,1679,1541,4278,4347)), 0)
+  expect_equal(sum(dt.count2$Region != c("Midwest","Midwest","Northeast","Northeast","South","South","West","West")), 0)
+  expect_equal(sum(dt.count2$Gender != c("Female", "Male","Female", "Male","Female", "Male","Female", "Male")), 0)
+  expect_equal(sum(dt.count2$`Records with Age > 65` != c(736,897,1403,1265,575,483,1564,1518)), 0)
 })
